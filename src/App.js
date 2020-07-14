840530ea4a41d1cc1c6a30477eb17d0cc10f1b18
Saba <sabaazam306@gmail.com>
2020-07-15 02:39:42 +0500

    Created bar chart for each country count

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
 
     state = {
         data: {},
+        country: '',
     }
 
     async componentDidMount() {
@@ -19,14 +20,22 @@ class App extends React.Component {
         this.setState({data:fetchedData});
     }
 
+    handleCountryChange = async (country) => {
+        // fetch the data
+        const fetchedData = await fetchData(country);
+
+        // set the state
+        this.setState({ data: fetchedData, country: country });
+    }
+
     render() {
-        const { data } = this.state;
+        const { data, country } = this.state;
 
         return(
             <div className={styles.container}>
                 <Cards data={data} />
-                <CountryPicker />
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
             </div>
         );
     }
